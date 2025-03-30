943e1f7022b8eca1e16b3daa753c7b4ec9a4b5a4
g453030291 <453030291@qq.com>
2025-03-30 18:05:13 +0800

    update:ocr

--- corki/api_views/ocr_views.py
+++ corki/api_views/ocr_views.py
@@ -15,7 +15,8 @@ class OCRViews(APIView):
         jd_url = data.get('jd_url')
         ocr_result = AliClient.ocr(jd_url)
         completion_response = doubao_client.completions(
-            system_prompts="下面是一个 ocr 识别的结果,我需要你去掉无意义的符号,帮我整理为json格式返回给我。分为两部分,职位名称和职位描述(工作职责,任职资格)"
+            system_prompts="下面是一个 ocr 识别的结果,我需要你去掉无意义的符号,帮我整理为json格式返回给我。分为两部分,职位名称和职位描述(工作职责,任职资格)。"
+                           "注意 jd_content 内只需要按(工作职责,任职资格)换行分隔即可,保持 jd_content 字符串的数据格式"
                            "output_format: json,"
                            "schema: {'jd_title':'', 'jd_content':''}",
             user_prompts="".join(ocr_result)
